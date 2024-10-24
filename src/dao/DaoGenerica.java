714660eab86532beab06c651414a5d4c5645cf6c
Marcelo <marcelofoztd@hotmail.com>
2024-10-24 19:58:34 -0300

    introduzido daogenerica

---
+++ src/dao/DaoGenerica.java
@@ -0,0 +1,21 @@
+/**
+ *
+ * @author xtremebass
+ */
+package dao;
+
+import java.util.ArrayList;
+
+public interface DaoGenerica<ObjetoGenerico> {
+    
+    public void inserir(ObjetoGenerico objt);
+    
+    public void alterar(ObjetoGenerico objt);
+    
+    public void excluir();
+    
+    public ArrayList<ObjetoGenerico> consultar();
+
+    public ArrayList<ObjetoGenerico> dashboard();    
+    
+}
